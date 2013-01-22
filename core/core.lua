927ae1bb9cbfb1e066c9d90f270b16fd945d97b6
repeat <repeat@mail333.com>
2013-01-22 09:55:19 -0800

    wrong place

--- core/core.lua
+++
@@ -1,8 +0,0 @@
-package.path = "scripts/core/?.lua;scripts/?.lua;?.lua"
-require "event_id"
-require "event"
-require "api"
-
-require "func"
-require "ginfo"
-require "timer"
--- trunk/!logo.lua
+++
@@ -1,3 +0,0 @@
-function logo(ucid)
-	luaLFS:btn(1, ucid, 1, 0, 32+1, 0, 193, 190, 7, 10, "RS")
-end
